5bda991f1604142e0889de99de1d5c36b0061736
Chakravarti-Pranjal <chakravartipranjal3@gmail.com>
2024-07-03 12:34:18 +0530

    routes & controllers initialized

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express()
 
-const PORT = 5001 ;
+const PORT = process.env.PORT || 5001 ;
+
+const product_routes = require('./routes/products')
 
 app.get("/", (req,res) => {
     res.send('Hello World')
 })
 
+// middleware or to set router
+app.use('/api/products', product_routes)
+
 app.listen(PORT, () => {
-    console.log('Server is running')
+    console.log(`Server is connecting on port: ${PORT}`)
 })
